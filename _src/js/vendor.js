b86678c092fbf51fb4f78e8833d65c24cdc73ab1
Sávio Mendes <saviomd@gmail.com>
2016-02-09 20:29:12 -0200

    Update bootstrap and other dependencies

--- _src/js/vendor.js
+++ _src/js/vendor.js
@@ -1,5 +1,6 @@
 module.exports = [
 	'node_modules/jquery/dist/jquery.js',
+	'node_modules/tether/dist/js/tether.min.js',
 	'node_modules/bootstrap/dist/js/bootstrap.js',
 	'node_modules/wow/dist/wow.js'
 ]
