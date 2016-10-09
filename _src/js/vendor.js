09b48903b04bbb2206fe87cdf8ed61b536768ef6
Sávio Mendes <saviomd@gmail.com>
2016-10-09 17:40:06 -0300

    Add manifest, update head, vendor.js and dependencies

--- _src/js/vendor.js
+++ _src/js/vendor.js
@@ -1,7 +1,7 @@
 module.exports = [
 	'node_modules/autotrack/autotrack.js',
 	'node_modules/jquery/dist/jquery.js',
-	'node_modules/tether/dist/js/tether.min.js',
-	'node_modules/bootstrap/dist/js/bootstrap.js',
+	'node_modules/bootstrap/js/dist/util.js',
+	'node_modules/bootstrap/js/dist/collapse.js',
 	'node_modules/wowjs/dist/wow.js'
 ]
