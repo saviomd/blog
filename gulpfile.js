8a862050a473001beadeb1fc1deb858575db5dce
Sávio Mendes <saviomd@gmail.com>
2018-01-25 17:04:39 -0200

    Update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var cssnano = require('cssnano');
 var cssnanoConfig = require('tools-config-saviomd/cssnano-config');
 var del = require('del');
 var eslint = require('gulp-eslint');
-var eslintConfig = require('tools-config-saviomd/eslint-config');
+var eslintConfig = './node_modules/tools-config-saviomd/eslint-config.js';
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var postcssFlexbugsFixes = require('postcss-flexbugs-fixes');
@@ -69,7 +69,7 @@ gulp.task('jsSite', function() {
 	return gulp.src(require('./_src/js/blog.js'))
 		.pipe(eslint(eslintConfig))
 		.pipe(eslint.format())
-		.pipe(eslint.failOnError())
+		.pipe(eslint.failAfterError())
 		.pipe(concat('blog.js'))
 		.pipe(gulp.dest('js'))
 		.pipe(uglify())
